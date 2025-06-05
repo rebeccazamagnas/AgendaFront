25952878ef742024fb23850c9c4d1a487d0c468b
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-06-05 13:10:47 -0300

    incluindo formulario no front

--- src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
+++ src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-consulta-tarefas',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './consulta-tarefas.component.html',
   styleUrl: './consulta-tarefas.component.css'
 })
 export class ConsultaTarefasComponent {
+
+  tarefas: any[] = [];
+
+  http = inject(HttpClient);
+
+  ngOnInit(){
+
+    this.http.get(environment.apiTarefas + '/tarefas')
+    .subscribe((response) => {
+      console.table(response);
+    })
+
+
+
+  }
 
 }
