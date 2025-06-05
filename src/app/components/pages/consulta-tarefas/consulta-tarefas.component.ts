028d1864fa4d8a8b824bb9bca3664ce3ae269598
rebeccazamagnas <rebeccasix@rocketmail.com>
2025-06-05 18:54:22 -0300

    ajustes na pagina de consulta

--- src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
+++ src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-tarefas',
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './consulta-tarefas.component.html',
   styleUrl: './consulta-tarefas.component.css'
@@ -16,16 +19,27 @@ export class ConsultaTarefasComponent {
   tarefas: any[] = [];
 
   http = inject(HttpClient);
+  fb = inject(FormBuilder);
 
-  ngOnInit(){
+  form = this.fb.group ({
+    dataMin : new FormControl ('', [Validators.required]),
+    dataMax : new FormControl('', [Validators.required])
 
-    this.http.get(environment.apiTarefas + '/tarefas')
+  });
+
+  onSubmit() {
+  const dataMin = this.form.value.dataMin;
+  const dataMax = this.form.value.dataMax;
+
+  this.http.get<any[]>(environment.apiTarefas + '/tarefas/' + dataMin + '/' + dataMax)
     .subscribe((response) => {
-      console.table(response);
-    })
+      this.tarefas = response;  // <-- Atualizar a variável para refletir no template
+      console.table(this.tarefas);
+    });
+}
 
 
 
   }
 
-}
+
